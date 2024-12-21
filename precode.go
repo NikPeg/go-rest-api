6997ec037a92f211ad7e6bf048ba23d53cdb27bd
NikPeg <peganov.nik@gmail.com>
2024-12-21 06:33:44 +0300

    delete

--- precode.go
+++ precode.go
@@ -97,26 +97,22 @@ func getTask(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
     w.Write(resp)
 }
-//
-// func deleteTask(w http.ResponseWriter, r *http.Request) {
-//     id := chi.URLParam(r, "id")
-//
-//     artist, ok := artists[id]
-//     if !ok {
-//         http.Error(w, "Артист не найден", http.StatusNoContent)
-//         return
-//     }
-//
-//     resp, err := json.Marshal(artist)
-//     if err != nil {
-//         http.Error(w, err.Error(), http.StatusBadRequest)
-//         return
-//     }
-//
-//     w.Header().Set("Content-Type", "application/json")
-//     w.WriteHeader(http.StatusOK)
-//     w.Write(resp)
-// }
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+    id := chi.URLParam(r, "id")
+
+    task, ok := tasks[id]
+    if !ok {
+        http.Error(w, "Задача не найдена", http.StatusBadRequest)
+        return
+    }
+
+    delete(tasks, id)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(resp)
+}
 
 func main() {
 	r := chi.NewRouter()
@@ -125,7 +121,7 @@ func main() {
 	r.Get("/tasks", getTasks)
 	r.Post("/tasks", postTasks)
 	r.Get("/tasks/{id}", getTask)
-// 	r.Delete("/tasks/{id}", deleteTask)
+	r.Delete("/tasks/{id}", deleteTask)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
